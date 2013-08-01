1a8f614fb897a28acab51babfc4eb81178b51eca
Florian Feldhaus <florian.feldhaus@gmail.com>
2013-08-01 14:48:43 +0200

    updates to mail view and login

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -2,6 +2,8 @@ class UserMailer < ActionMailer::Base
   default :from => 'florian.feldhaus@gmail.com'
 
   def new_game_confirmation(supervisor, game)
+    @supervisor = supervisor
+    @game = game
     mail(:to => supervisor.email, :subject => I18n.t('email_new_game_subject' + ' ' + game.name))
   end
 end
