93980376169b7b4eb49da0161df062bba5e45a7d
Florian Feldhaus <florian.feldhaus@gmail.com>
2013-08-01 14:18:40 +0200

    removed default email address

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,5 +1,4 @@
 class UserMailer < ActionMailer::Base
-  default from: "florian.feldhaus@gmail.com"
 
   def new_game_confirmation(supervisor, game)
     mail(:to => supervisor.email, :subject => I18n.t('email_new_game_subject' + ' ' + game.name))
